1d85e070c071a440cca3f72fd2c84f6595ef30fa
aanantharam <aanantharam@hawk.iit.edu>
2020-06-12 23:35:36 -0700

    Hyperlink-checker
    This program checks if a particular hyperlink is valid. If an invalid link is provided, mentions the particular type of invalid link.
    Further updates- obtain secondary links from primary websites links.

---
+++ hyperlink.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.error
+
+url = input()
+try:
+    conn = urllib.request.urlopen(url)
+except urllib.error.HTTPError as e:
+    # Returns the particular error in the code (e.g. 404, 501, ...)
+    print('There seems to be an error with the page: HTTPError: {}'.format(e.code))
+except urllib.error.URLError as e:
+    # Not an HTTP-specific error (e.g. connection refused)
+    print('There seems to be an error with the page: URLError: {}'.format(e.reason))
+else:
+    # 200
+    print('Hyperlink looks good! ')
+
+
+ 
